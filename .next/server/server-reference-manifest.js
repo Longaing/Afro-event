9da3d57aff59800db29b44f403e47066a0e019d3
Longaing <longaing.guy16@gmail.com>
2025-07-29 16:43:48 +0200

    Build for GitHub Pages

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"11NHwOFllv7yg7McuqDgLdiFMGXwLx63yjgOSnApj+c=\"\n}"
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-const repo = 'Afro-event'; // Remplace par le nom de ton repo si besoin
-
 const nextConfig: NextConfig = {
   output: 'export',
-  // Si tu déploies sur une sous-arborescence, décommente la ligne suivante :
-  // basePath: isGithubPages ? `/${repo}` : '',
-  // assetPrefix: isGithubPages ? `/${repo}/` : '',
+  basePath: '/Afro-event',      // Nom exact du dépôt
+  assetPrefix: '/Afro-event/',  // Préfixe pour les assets
 };
 
 export default nextConfig;
