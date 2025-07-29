f09d3441140d85757784a48137b7c322dc563318
arnaud <longaing.guy16@gmail.com>
2025-07-29 17:27:09 +0200

    Create next.config.js

---
+++ .github/workflows/next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+module.exports = {
+  output: 'export',
+  basePath: '/Afro-event',
+  assetPrefix: '/Afro-event/',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+}
