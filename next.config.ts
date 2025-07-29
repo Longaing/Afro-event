dd69ac4dcbe4ccee73f1a65f34bd1b795cfe0b23
Longaing <longaing.guy16@gmail.com>
2025-07-29 16:15:07 +0200

    add

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
+const repo = 'Afro-event'; // Remplace par le nom de ton repo si besoin
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Si tu déploies sur une sous-arborescence, décommente la ligne suivante :
+  // basePath: isGithubPages ? `/${repo}` : '',
+  // assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
